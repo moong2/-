17108d4ea0385f3bd287490a6878576af6c5c919
뭉이 <clapmean@gmail.com>
2020-12-21 15:07:33 +0900

    fadein

--- login.js
+++ login.js
@@ -1,19 +1,11 @@
-var backgroundImg = new Array();
-backgroundImg[0] = "1.jpg";
-backgroundImg[1] = "2.jpg";
-backgroundImg[2] = "3.jpg";
-backgroundImg[3] = "4.jpg";
-backgroundImg[4] = "5.jpg";
-backgroundImg[5] = "6.jpg";
-backgroundImg[6] = "7.jpg";
-backgroundImg[7] = "8.jpg";
-backgroundImg[8] = "9.jpg";
-backgroundImg[9] = "10.jpg";
+var now_img, next_img;
+ function fade_change(){
+     now_img = $(".fade_container img:eq(0)");
+     next_img = $(".fade_container img:eq(1)");
+     next_img.addClass("active").css("opacity",0).animate({"opacity":1},1000, function(){
+         $(".fade_container").append(now_img);
+         now_img.removeClass("active");
+     });
+ }
 
-function showImage(){
-    var imgNum = Math.round(Math.random()*9);
-    var objImg = document.getElementById("introImg");
-    objImg.src = backgroundImg[imgNum];
-
-    setTimeout("showImage()", 5000);
-}
+ var timer = setInterval("fade_change()", 4000);
